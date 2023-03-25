4ba92fb7f06a0a762954019e556fa1501feac360
Syuugo <pub@s1204.me>
2023-03-25 23:37:53 +0900

    最適化

--- app/FirmwareUpdate/src/main/java/com/panasonic/sanyo/ts/firmwareupdate/FirmwareUpdateActivity.java
+++ app/FirmwareUpdate/src/main/java/com/panasonic/sanyo/ts/firmwareupdate/FirmwareUpdateActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class FirmwareUpdateActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    super.onDestroy();
     finish();
   }
 }
