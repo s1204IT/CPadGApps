84258d777b0d7788f65af99c996668ea9d54d4b5
Syuugo <pub@s1204.me>
2023-06-10 00:33:48 +0900

    Updates

--- app/FirmwareUpdate/src/main/java/com/panasonic/sanyo/ts/firmwareupdate/FirmwareUpdateActivity.java
+++ app/FirmwareUpdate/src/main/java/com/panasonic/sanyo/ts/firmwareupdate/FirmwareUpdateActivity.java
@@ -6,6 +6,6 @@ import android.os.Bundle;
 public class FirmwareUpdateActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    finish();
+    finishAndRemoveTask();
   }
 }
